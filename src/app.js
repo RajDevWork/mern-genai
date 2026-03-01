30752b64a86b001c001ed4ddfb8919abff662bf2
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-03-01 07:27:31 +0530

    initializing auth route for auth related routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 app = express()
 
+/**Requiring all the routes */
+const authRouter = require("./routes/auth.route")
 
+/**Using all the routes here */
+app.use("/api/auth",authRouter)
 
 module.exports = app
