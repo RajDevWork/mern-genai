ce8d9ac6511d633cf907495c23383a9bc75b911d
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-03-01 15:41:46 +0530

    Login and register page UI created

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../auth.form.scss'
+import { Link } from 'react-router'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main>
+        <div className='form-container'>
+            <h1>Login</h1>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder='Enter email'/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder='Enter password'/>
+                </div>
+                <button type="button" className='button primary-button'>Login</button>
+            </form>
+            <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
+        </div>
+    </main>
   )
 }
 
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <main>
+        <div className='form-container'>
+            <h1>Register</h1>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" placeholder='Enter username'/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder='Enter email'/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder='Enter password'/>
+                </div>
+                <button type="button" className='button primary-button'>Register</button>
+            </form>
+            <p>Already have an account? <Link to={"/login"}>Login</Link></p>
+        </div>
+    </main>
   )
 }
 
