c68116375d6861892006e45302e59524fe97bb8b
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-03-02 12:48:19 +0530

    UI setup for login/register with 4 layers arch

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../auth.form.scss'
-import { Link } from 'react-router'
-
+import { Link, useNavigate } from 'react-router'
+import { useAuth } from '../Hooks/useAuth'
 const Login = () => {
+    const {IsLoading,handleLogin} = useAuth()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        await handleLogin({email,password})
+        navigate("/")
+
+    }
+
   return (
     <main>
         <div className='form-container'>
+            {
+                IsLoading? <h1>Loading...</h1>:''
+            }
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder='Enter email'/>
+                    <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} name="email" id="email" placeholder='Enter email'/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder='Enter password'/>
+                    <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} name="password" id="password" placeholder='Enter password'/>
                 </div>
-                <button type="button" className='button primary-button'>Login</button>
+                <button type="submit" className='button primary-button'>Login</button>
             </form>
             <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
         </div>
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router'
+import { useAuth } from '../Hooks/useAuth'
 
 const Register = () => {
+    const {IsLoading,handleRegister} = useAuth()
+    const navigate = useNavigate()
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+        await handleRegister({username,email,password})
+        navigate("/login")
+
+    }
+
   return (
     <main>
         <div className='form-container'>
+            {
+                IsLoading? <h1>Loading...</h1>:''
+            }
             <h1>Register</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" placeholder='Enter username'/>
+                    <input type="text" onChange={(e)=>setUsername(e.target.value)} value={username} name="username" id="username" placeholder='Enter username'/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder='Enter email'/>
+                    <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} name="email" id="email" placeholder='Enter email'/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder='Enter password'/>
+                    <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} name="password" id="password" placeholder='Enter password'/>
                 </div>
-                <button type="button" className='button primary-button'>Register</button>
+                <button type="submit" className='button primary-button'>Register</button>
             </form>
             <p>Already have an account? <Link to={"/login"}>Login</Link></p>
         </div>
